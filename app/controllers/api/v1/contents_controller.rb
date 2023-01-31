4ecf40ba82b0488f4781ef97ac1bd63cd40ee993
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-01 02:08:04 +0545

    Content controller file

---
+++ app/controllers/api/v1/contents_controller.rb
@@ -0,0 +1,64 @@
+class Api::V1::ContentsController < ApplicationController
+  before_action :authorize, only: %i[create destroy update]
+  before_action :set_content, only: %i[:update destroy]
+
+  def index
+    @project = Project.find(params[:project_id])
+    @content = @project.contents.order("created_at desc")
+    render json: ContentSerializer.new(@content), status: 200
+  end
+
+  def create
+    @project = Project.find(params[:project_id]) if params[:project_id]
+    if @project.user.id.eql? logged_in_user.id
+      @content = Content.new(content_params.merge({project_id: @project.id, user_id: logged_in_user.id}))
+      if @content.save!
+        render json: ContentSerializer.new(@content), status: 201
+      else
+        render json: @content.errors, status: :unprocessable_entity
+      end
+    else
+      render json: {error: "permission denied."} , status: :unauthorized
+    end
+  end
+
+  def update
+    if @content.user.id.eql? logged_in_user.id
+      if @content.update(content_params)
+        render json: ContentSerializer.new(@content), status: 200
+      else
+        render json: @content.errors, status: :unprocessable_entity
+      end
+    else
+      render json: {error: "permission denied."}, status: :unauthorized
+    end
+  end
+
+  def show
+    @project = Project.find(params[:project_id]) if params[:project_id]
+    if @project.user.id.eql? logged_in_user&.id
+      @content = @project.contents.find_by(id: params[:id])
+      render json: ContentSerializer.new(@content), status: 200
+    else
+      render json: {error:"permission denied."}, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if @content.user.id.eql? logged_in_user.id
+      @content.destroy
+      render json: {message: "Deleted"}, status: 200
+    else
+      render json: {message: "permission denied."}, status: :unauthorized
+    end
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(:title, :body, :project_id)
+  end
+
+  def set_content
+    @content = Content.find(params[:id])
+  end
+end
