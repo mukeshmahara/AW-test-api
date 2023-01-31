23b9697a764f3b86b8f6fdb3ef0edef38dbd878e
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-01 02:10:04 +0545

    Added User Controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authorize, except: %i[create]
+  # before_action :authorize, only: [:auto_login]
+
+  
+  # REGISTER
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @token = encode_token({user_id: @user.id})
+      render json: UserSerializer.new( @user, {params: {token: @token, name: @user.full_name} }), status: 201
+    else
+      render json: {error: @user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :country, :password, :email)
+  end
+
+end
